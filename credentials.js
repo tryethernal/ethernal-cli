f6672705ded5a19508328ba95b7159d2a9fdf0de
Antoine de Chevigné <antoine@bannerman.com>
2021-08-03 16:17:36 +0200

    allow logith with env vars

--- credentials.js
+++ credentials.js
@@ -1,8 +1,8 @@
 const keytar = require('keytar');
 const Configstore = require('configstore');
 
-const CONFIGSTORE_EMAIL_KEY = process.env.NODE_ENV === 'development' ? 'ethernal-dev.email' : 'ethernal.email';
-const KEYCHAIN_NAMESPACE = process.env.NODE_ENV === 'development' ? 'ethernal-dev:firebase' : 'ethernal:firebase';
+const CONFIGSTORE_EMAIL_KEY = "ethernal.email";
+const KEYCHAIN_NAMESPACE = "ethernal:firebase";
 
 const configstore = new Configstore(CONFIGSTORE_EMAIL_KEY);
 
@@ -16,10 +16,10 @@ var _setPassword = async function(email, password) {
 
 module.exports = {
     getEmail: async () => {
-        return await configstore.get(CONFIGSTORE_EMAIL_KEY);
+        return process.env.ETHERNAL_EMAIL ? await new Promise((resolve) => resolve(process.env.ETHERNAL_EMAIL)) : await configstore.get(CONFIGSTORE_EMAIL_KEY);
     },
     getPassword: async (email) => {
-        return await keytar.getPassword(KEYCHAIN_NAMESPACE, email);
+        return process.env.ETHERNAL_PASSWORD ? await new Promise((resolve) => resolve(process.env.ETHERNAL_PASSWORD)) : await keytar.getPassword(KEYCHAIN_NAMESPACE, email);
     },
     set: async (email, password) => {
         await _setEmail(email);
